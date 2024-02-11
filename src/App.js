8768bc0c718e5b4b7983cdc92ae214cf2e34c864
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 15:20:55 -0500

    importing signup form to see if it looks right

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SignInUp from './components/SignInUp';
 
 function App() {
   return (
     <div className="App">
       <h1>Blog App</h1>
+      <SignInUp/>
     </div>
   );
 }
