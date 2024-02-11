97fd791f44f3663e1caf3562eeb24d123273e796
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 15:21:46 -0500

    forms for signin or signup

--- src/components/SignInUp.js
+++ src/components/SignInUp.js
@@ -1,9 +1,82 @@
 function SignInUp(props) {
-    return(
-        <div>
-            <h1>Sign in or sign up component</h1>
-        </div>
-    );
+  return (
+    <>
+    <div>
+        {/* sign up form */}
+      <form
+        // onSubmit={handleSignUp}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "10px",
+        }}
+      >
+        <h3>Sign Up</h3>
+
+        <input
+          //   ref={emailInputRef}
+          name="email"
+          id="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <input name="username" type="text" placeholder="Username" required />
+        <input
+          //   ref={}
+          name="password"
+          id="password"
+          type="text"
+          placeholder="Password"
+        />
+        <input
+          id="passwordCheck"
+          name="passwordCheck"
+          type="text"
+          placeholder="Repeat Password"
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+      <span>
+        Already have an account? <button>Sign In</button>
+      </span>
+    </div>
+    {/* sign in form */}
+    <div>
+      <form
+        // onSubmit={handleSignUp}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "10px",
+        }}
+      >
+        <h3>Sign In</h3>
+
+        <input
+          //   ref={emailInputRef}
+          name="emailOrUsername"
+          id="emailOrUsername"
+          type="text"
+          placeholder="Email or Username"
+          required
+        />
+        <input
+          //   ref={}
+          name="password"
+          id="password"
+          type="text"
+          placeholder="Password"
+        />
+
+        <button type="submit">Sign In</button>
+      </form>
+      <span>
+        Don't have an account? <button>Sign Up</button>
+      </span>
+    </div>
+    </>
+  );
 }
 
 export default SignInUp;
