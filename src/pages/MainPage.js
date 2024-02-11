528dd6b3bdd38adef9094d4f69c5939b6306ca7b
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 18:55:09 -0500

    boilerplate

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import SignInUp from "../components/SignInUp";
+
+function MainPage(){
+    return (
+        <div>
+            <h1>main page</h1>
+            <SignInUp/>
+        </div>
+    )
+}
+
+export default MainPage;
