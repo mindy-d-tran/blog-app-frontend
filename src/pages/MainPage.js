1c596c43b5ca474003964917d51eb0c5b50ed0da
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 12:47:39 -0500

    add class name

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,14 @@
 import SignInUp from "../components/SignInUp";
 import Logo from "../assets/bear_logo_launch.png";
 
-import './MainPage.css';
+import "./MainPage.css";
 
 function MainPage() {
   return (
     <div className="launch-page">
-      <div >
+      <div className="hero-area">
         <img className="logo-big" src={Logo} alt="logo big" />
-        <h1>main page</h1>
+        <h1>Blog App</h1>
       </div>
       <SignInUp />
     </div>
