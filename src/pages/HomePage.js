bd547445c3c1752ed4b9317d220e2fa9f186e819
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 21:40:05 -0500

    display navbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
+import NavBar from "../components/NavBar";
 import PostList from "../components/PostList";
 
 function HomePage() {
   return (
     <>
       <h1>home page</h1>
+      <NavBar/>
       <PostList />
     </>
   );
