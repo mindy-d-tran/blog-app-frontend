104c1e8ca30a3e4bf954157efac5de8602f5294e
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 17:19:08 -0500

    revert change

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,34 @@
 import SignInUp from "../components/SignInUp";
-import Logo from "../assets/bear_logo_launch.png";
+import Logo from "../assets/cat_logo.png";
+
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
 
 import "./MainPage.css";
+import HomePage from "./HomePage";
 
 function MainPage() {
+  const userCtx = useContext(UserContext);
+  const { user } = userCtx;
+
   return (
-    <div className="launch-page">
-      <div className="hero-area">
-        <img className="logo-big" src={Logo} alt="logo big" />
-        <h1>Blog App</h1>
-      </div>
-      <SignInUp />
-    </div>
+    <>
+      {user ? (
+        <>
+          <HomePage />
+        </>
+      ) : (
+        <>
+          <div className="launch-page">
+            <div className="hero-area">
+              <img className="logo-big" src={Logo} alt="logo big" />
+              <h1>Blog App</h1>
+            </div>
+            <SignInUp />
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
