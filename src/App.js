483698de6c394a430735b6bd91dbddd54f34a21e
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 19:52:27 -0500

    create routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
+import HomePage from './pages/HomePage';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Blog App</h1>
-      <MainPage/>
+      <Routes>
+        <Route path='/' element={<MainPage/>} />
+        <Route path='/home' element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
