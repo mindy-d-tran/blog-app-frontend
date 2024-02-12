4cc859df29db3bf72911226d98156c531164ce58
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 10:50:52 -0500

    import img

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,12 @@
 import SignInUp from "../components/SignInUp";
-
+import Logo from '../assets/bear_logo_transparent.png'
 function MainPage(){
     return (
         <div>
+            <div>
             <h1>main page</h1>
+                <img className="logo-big" src={Logo} alt="logo big" />
+            </div>
             <SignInUp/>
         </div>
     )
