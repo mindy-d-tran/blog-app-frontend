12475d7ac1730efd60bd26f53a06309a634e76c8
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 15:18:16 -0500

    add usercontext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+// importing hooks
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+
+// importing style
+import "./App.css";
+
+// importing context
+import { UserContext } from "./context/UserContext";
 
 // importing pages
-import MainPage from './pages/MainPage';
-import HomePage from './pages/HomePage';
-import SettingPage from './pages/SettingPage';
+import MainPage from "./pages/MainPage";
+import HomePage from "./pages/HomePage";
+import SettingPage from "./pages/SettingPage";
 
 //importing components
-import NavBar from './components/NavBar';
-
+import NavBar from "./components/NavBar";
 
 function App() {
+  // creeating state for user
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<MainPage/>} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/setting' element={<SettingPage/>} />
-      </Routes>
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/setting" element={<SettingPage />} />
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
