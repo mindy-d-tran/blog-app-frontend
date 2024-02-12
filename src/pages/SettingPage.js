59adcba75ced5f07165578a30a851d1393c6be7b
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 18:34:38 -0500

    import font awsome and add state to edit account

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -1,20 +1,65 @@
+import { UserContext } from "../context/UserContext";
+import { useContext, useState } from "react";
+
+// importing icons from font awsome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
+import './SettingPage.css';
+// const API_KEY ="live_q6c3JrM10gOEfZinLjnSxMkjOdqR1bToljv8rziaj3d2JoXRsFRvb1AaEUzKKVQ2";
+// const url = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`;
+
 function SettingPage() {
+  const userCtx = useContext(UserContext);
+  const { user } = userCtx;
+  console.log(user);
+
+  const [editProfilePic, setEditProfilePic] = useState(false);
+  const [editUsername, setEditUsername] = useState(false);
+  const [editEmail, setEditEmail] = useState(false);
+
+  const handleEditProfilePic = () => {
+    setEditProfilePic(true);
+    console.log(editProfilePic);
+  };
+  const handleEditUsername = () => {
+    setEditUsername(!editUsername);
+
+  };
+
   return (
     <div>
-      <h2>Setting Page</h2>
+      <h2>Settings</h2>
       <div>
-        <img src="" alt="profile pic" />
-        <p>Username</p>
+        <img onClick={handleEditProfilePic} src="" alt="profile pic" />
+        {editUsername ? (
+          <form>
+            {" "}
+            <input type="text" />
+          </form>
+        ) : (
+          <p>
+            {user.username}
+          </p>
+        )}
+        <FontAwesomeIcon icon={faPen} onClick={handleEditUsername}/>
       </div>
       <form>
-        <label> Email
-            <input type="text" placeholder="Email" />
-        </label>
+        <label> Email </label>
+        {editEmail ? (
+          <input type="text" placeholder="Email" />
+        ) : (
+          <p>
+            {user.email} <FontAwesomeIcon icon={faPen} />
+          </p>
+        )}
       </form>
       <form>
-        <label> Password
-            <input type="text" placeholder="Current Password" />
-            <input type="text" placeholder="New Password" />
+        <label>
+          {" "}
+          Password
+          <input type="text" placeholder="Current Password" />
+          <input type="text" placeholder="New Password" />
         </label>
       </form>
     </div>
