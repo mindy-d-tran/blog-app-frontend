41462cc2879cab50aa26722b414cadfcd6b5a664
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 11:38:25 -0500

    change logo and add class names

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,18 @@
 import SignInUp from "../components/SignInUp";
-import Logo from '../assets/bear_logo_transparent.png'
-function MainPage(){
-    return (
-        <div>
-            <div>
-            <h1>main page</h1>
-                <img className="logo-big" src={Logo} alt="logo big" />
-            </div>
-            <SignInUp/>
-        </div>
-    )
+import Logo from "../assets/bear_logo_launch.png";
+
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div className="launch-page">
+      <div >
+        <img className="logo-big" src={Logo} alt="logo big" />
+        <h1>main page</h1>
+      </div>
+      <SignInUp />
+    </div>
+  );
 }
 
 export default MainPage;
