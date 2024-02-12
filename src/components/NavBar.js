c046103506eb56d1dc7d8330fe2b59c1303ac066
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 21:35:48 -0500

    something temp for now

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li></li>
+        <li>About</li>
+      </ul>
+      <form>
+        <input type="text" placeholder="Search" />
+      </form>
+    </nav>
+  );
+}
+
+export default NavBar;
