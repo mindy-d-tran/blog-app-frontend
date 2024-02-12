06699c7fbb1b8bc61d51e501bc6fc133ed9c0fee
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 09:58:10 -0500

    temp adding nav to app commonent so it's easier to check the other pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// importing pages
 import MainPage from './pages/MainPage';
 import HomePage from './pages/HomePage';
+import SettingPage from './pages/SettingPage';
+
+//importing components
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Blog App</h1>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<MainPage/>} />
         <Route path='/home' element={<HomePage />} />
+        <Route path='/setting' element={<SettingPage/>} />
       </Routes>
     </div>
   );
