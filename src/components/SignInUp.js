94f4aad01776a9cf963e5f6901f9f7d46fd058a8
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 12:35:16 -0500

    add use state to change form to sign in or sign up

--- src/components/SignInUp.js
+++ src/components/SignInUp.js
@@ -1,81 +1,100 @@
+import { useState } from "react";
+
 function SignInUp(props) {
+  // state to change form to sign in or sign up
+  const [createAccount, setCreateAccount] = useState(true);
+
   return (
-    <>
-    <div>
-        {/* sign up form */}
-      <form
-        // onSubmit={handleSignUp}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "10px",
-        }}
-      >
-        <h3>Sign Up</h3>
+    <section>
+      {createAccount ? (
+        // signup form
+        <div>
+          <form
+            // onSubmit={handleSignUp}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+            }}
+          >
+            <h3>Sign Up</h3>
 
-        <input
-          //   ref={emailInputRef}
-          name="email"
-          id="email"
-          type="email"
-          placeholder="Email"
-          required
-        />
-        <input name="username" type="text" placeholder="Username" required />
-        <input
-          //   ref={}
-          name="password"
-          id="password"
-          type="text"
-          placeholder="Password"
-        />
-        <input
-          id="passwordCheck"
-          name="passwordCheck"
-          type="text"
-          placeholder="Repeat Password"
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-      <span>
-        Already have an account? <button>Sign In</button>
-      </span>
-    </div>
-    {/* sign in form */}
-    <div>
-      <form
-        // onSubmit={handleSignUp}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "10px",
-        }}
-      >
-        <h3>Sign In</h3>
+            <input
+              //   ref={emailInputRef}
+              name="email"
+              id="email"
+              type="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              name="username"
+              type="text"
+              placeholder="Username"
+              required
+            />
+            <input
+              //   ref={}
+              name="password"
+              id="password"
+              type="text"
+              placeholder="Password"
+            />
+            <input
+              id="passwordCheck"
+              name="passwordCheck"
+              type="text"
+              placeholder="Repeat Password"
+            />
+            <button type="submit">Sign Up</button>
+          </form>
+          <span>
+            Already have an account?{" "}
+            <button onClick={() => setCreateAccount(!createAccount)}>
+              Sign In
+            </button>
+          </span>
+        </div>
+      ) : (
+        // sign in form
+        <div>
+          <form
+            // onSubmit={handleSignUp}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+            }}
+          >
+            <h3>Sign In</h3>
 
-        <input
-          //   ref={emailInputRef}
-          name="emailOrUsername"
-          id="emailOrUsername"
-          type="text"
-          placeholder="Email or Username"
-          required
-        />
-        <input
-          //   ref={}
-          name="password"
-          id="password"
-          type="text"
-          placeholder="Password"
-        />
+            <input
+              //   ref={emailInputRef}
+              name="emailOrUsername"
+              id="emailOrUsername"
+              type="text"
+              placeholder="Email or Username"
+              required
+            />
+            <input
+              //   ref={}
+              name="password"
+              id="password"
+              type="text"
+              placeholder="Password"
+            />
 
-        <button type="submit">Sign In</button>
-      </form>
-      <span>
-        Don't have an account? <button>Sign Up</button>
-      </span>
-    </div>
-    </>
+            <button type="submit">Sign In</button>
+          </form>
+          <span>
+            Don't have an account?{" "}
+            <button onClick={() => setCreateAccount(!createAccount)}>
+              Sign Up
+            </button>
+          </span>
+        </div>
+      )}
+    </section>
   );
 }
 
