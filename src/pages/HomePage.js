7a27eeb97910304f75cb43fdbce5cba424004ff2
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 19:56:03 -0500

    boilerplate and some code

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import PostList from "../components/PostList";
+
+function HomePage() {
+  return (
+    <>
+      <h1>home page</h1>
+      <PostList />
+    </>
+  );
+}
+
+export default HomePage;
