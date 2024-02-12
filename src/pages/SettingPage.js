d93e6c2f572c27fa1c923ef871fb1509117f33ec
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 10:50:29 -0500

    add elements for settings

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -1,9 +1,24 @@
-function SettingPage () {
-    return (
-        <div>
-            <h2>Setting Page</h2>
-        </div>
-    )
+function SettingPage() {
+  return (
+    <div>
+      <h2>Setting Page</h2>
+      <div>
+        <img src="" alt="profile pic" />
+        <p>Username</p>
+      </div>
+      <form>
+        <label> Email
+            <input type="text" placeholder="Email" />
+        </label>
+      </form>
+      <form>
+        <label> Password
+            <input type="text" placeholder="Current Password" />
+            <input type="text" placeholder="New Password" />
+        </label>
+      </form>
+    </div>
+  );
 }
 
 export default SettingPage;
