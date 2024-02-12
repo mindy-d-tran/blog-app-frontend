375ab8f0081c6f353abc4c99398649b3430157ca
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 09:01:52 -0500

    alt temp alt text and temp post content

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,10 @@
 function Post(props){
     return (
         <div className="post">
-            <img className="user-post-icon" src="" alt="" />
+            <img className="user-post-icon" src="" alt="user icon" />
             <p>Username</p>
-            <p className="post-content"></p>
-            <img className="post-img"  src="" alt="" />
+            <p className="post-content">post content</p>
+            <img className="post-img"  src="" alt="post img" />
         </div>
     );
 }
