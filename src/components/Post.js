69fdea76ad1c7ce2dfcc40ccfb07bb211c747239
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 09:36:44 -0500

    import comment for testing

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,27 @@
-function Post(props){
-    return (
-        <div className="post">
-            <img className="user-post-icon" src="" alt="user icon" />
-            <p>Username</p>
-            <h2>post Title</h2>
-            <img className="post-img"  src="" alt="post img" />
-            <p className="post-content">post content</p>
-            <p>hash tags</p>
-            <section className="postbuttons" >
-                <button className="like" >like</button>
-                <button className="comment" >comment</button>
-                <button className="share" >share</button>
-            </section>
-        </div>
-    );
+import Comment from "./Comment";
+
+function Post(props) {
+  return (
+    <div>
+      <section className="post">
+        <img className="user-post-icon" src="" alt="user icon" />
+        <p>Username</p>
+        <h2>post Title</h2>
+        <img className="post-img" src="" alt="post img" />
+        <p className="post-content">post content</p>
+        <p>hash tags</p>
+        <section className="postbuttons">
+          <button className="like">like</button>
+          <button className="comment">comment</button>
+          <button className="share">share</button>
+        </section>
+      </section>
+      
+      <section className="comment-list">
+        <Comment/>
+      </section>
+    </div>
+  );
 }
 
 export default Post;
