69c87cbeead489984658d8acbfb4895558d51ad6
Mindy D Tran <mindytran98@gmail.com>
2024-02-11 19:55:49 -0500

    boilerplate

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+function Post(props){
+    return (
+        <div className="post">
+            <img className="user-post-icon" src="" alt="" />
+            <p>Username</p>
+            <p className="post-content"></p>
+            <img className="post-img"  src="" alt="" />
+        </div>
+    );
+}
+
+export default Post;
---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import Post from "./Post";
+
+function PostList(){
+    return (
+        <section>
+            <Post />
+        </section>
+    );
+}
+
+export default PostList;
