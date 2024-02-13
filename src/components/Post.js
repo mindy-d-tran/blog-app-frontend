cebabed7b694075e7bbc4c28031e3db29e67124c
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 23:20:11 -0500

    showing info from post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,54 @@
 import Comment from "./Comment";
+// importing icons from font awsome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart,
+  faComment,
+  faShareFromSquare,
+} from "@fortawesome/free-regular-svg-icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function Post(props) {
+function Post({ post }) {
+  const [postUser, setPostUser] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `http://localhost:4000/api/users/${post.user_id}`
+      );
+      setPostUser(res.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <section className="post">
-        <img className="user-post-icon" src="" alt="user icon" />
-        <p>Username</p>
-        <h2>post Title</h2>
-        <img className="post-img" src="" alt="post img" />
-        <p className="post-content">post content</p>
-        <p>hash tags</p>
-        <section className="postbuttons">
-          <button className="like">like</button>
-          <button className="comment">comment</button>
-          <button className="share">share</button>
-        </section>
-      </section>
-      
-      <section className="comment-list">
-        <Comment/>
-      </section>
-    </div>
+    <>
+      {postUser ? (
+        <div>
+          <section className="post">
+            <img className="user-post-icon" src="" alt="user icon" />
+            <p>{postUser.username}</p>
+            <h2>{post.post_title}</h2>
+            {post.post_content.img ? <img className="post-img" src="" alt="post img" />:<></>}
+            
+            <p className="post-content">{post.post_content.text}</p>
+            <p>hash tags</p>
+            <section className="postbuttons">
+              <FontAwesomeIcon icon={faHeart} />
+              <FontAwesomeIcon icon={faComment} />
+              <FontAwesomeIcon icon={faShareFromSquare} />
+            </section>
+          </section>
+
+          <section className="comment-list">
+            <Comment />
+          </section>
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
+    </>
   );
 }
 
