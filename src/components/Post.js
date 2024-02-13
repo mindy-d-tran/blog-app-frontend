1346d5199dd7d5c609e4df2348ea63ce1827fae0
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 10:55:03 -0500

    make like and unlike work properly

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,41 +1,77 @@
 import Comment from "./Comment";
 // importing icons from font awsome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import {
-  faHeart,
+  faHeart as fasHeart,
   faComment,
   faShareFromSquare,
 } from "@fortawesome/free-regular-svg-icons";
-import { useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
+import { UserContext } from "../context/UserContext";
+
+import { Link } from "react-router-dom";
 
 function Post({ post }) {
-  const [postUser, setPostUser] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(
-        `http://localhost:4000/api/users/${post.user_id}`
-      );
-      setPostUser(res.data);
-    };
-    fetchData();
-  }, []);
 
+  const userCtx = useContext(UserContext);
+  const { user } = userCtx;
+
+  const [singlePost, setSinglePost] = useState(post);
+
+  const handleOnClickLike = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/posts/${singlePost._id}/unlike`,
+        { user_id: user._id }
+      );
+      console.log(res.data);
+      setSinglePost(res.data.post);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleOnClickUnlike = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/posts/${singlePost._id}/like`,
+        { user_id: user._id }
+      );
+      console.log(res.data);
+      setSinglePost(res.data.post);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      {postUser ? (
-        <div>
-          <section className="post">
+      {singlePost ? (
+        <div className="post">
+          <section className="post-body">
             <img className="user-post-icon" src="" alt="user icon" />
-            <p>{postUser.username}</p>
-            <h2>{post.post_title}</h2>
-            {post.post_content.img ? <img className="post-img" src="" alt="post img" />:<></>}
-            
-            <p className="post-content">{post.post_content.text}</p>
+            <p>{singlePost.user_id.username}</p>
+            <Link to={`/post/${post._id}`}>
+              <h2>{singlePost.post_title}</h2>
+              {singlePost.post_content.img ? (
+                <img className="post-img" src="" alt="post img" />
+              ) : (
+                <></>
+              )}
+
+              <p className="post-content">{singlePost.post_content.text}</p>
+            </Link>
+
             <p>hash tags</p>
             <section className="postbuttons">
-              <FontAwesomeIcon icon={faHeart} />
+              {singlePost.post_likes.find((p) => p.user_id === user._id) ? (
+                <FontAwesomeIcon onClick={handleOnClickLike} icon={faHeart} />
+              ) : (
+                <FontAwesomeIcon
+                  onClick={handleOnClickUnlike}
+                  icon={fasHeart}
+                />
+              )}
               <FontAwesomeIcon icon={faComment} />
               <FontAwesomeIcon icon={faShareFromSquare} />
             </section>
