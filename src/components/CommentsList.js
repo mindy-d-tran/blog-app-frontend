df344da9fc05c3f0fbb5d486ebd27e99cf3ce265
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 11:47:47 -0500

    mapping comments

---
+++ src/components/CommentsList.js
@@ -0,0 +1,11 @@
+function CommentsList({ comments }) {
+  return (
+    <section className="comment-list">
+      {comments.map((c) => (
+        <Comment key={c._id} comment={c} />
+      ))}
+    </section>
+  );
+}
+
+export default CommentsList;
