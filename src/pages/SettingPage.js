717d4a7840df3c2f45a533bb5f72ca6b05fabfb3
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 19:51:46 -0500

    user can edit username

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -1,33 +1,72 @@
 import { UserContext } from "../context/UserContext";
+
 import { useContext, useState } from "react";
+import axios from "axios";
 
 // importing icons from font awsome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
+
 import TempProfilePic from "../assets/cat_logo_transparent.png";
 
+// importing style
 import "./SettingPage.css";
+
 // const API_KEY ="live_q6c3JrM10gOEfZinLjnSxMkjOdqR1bToljv8rziaj3d2JoXRsFRvb1AaEUzKKVQ2";
 // const url = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`;
 
 function SettingPage() {
   const userCtx = useContext(UserContext);
-  const { user } = userCtx;
-  //   console.log(user);
+  const { user, setUser } = userCtx;
 
+  // states to edit account fields
   const [editProfilePic, setEditProfilePic] = useState(false);
   const [editUsername, setEditUsername] = useState(false);
   const [editEmail, setEditEmail] = useState(false);
 
-  const handleEditProfilePic = () => {
+  // states to handle change in input
+  const [username, setUsername] = useState(user.username);
+  const [email, setEmail] = useState(user.email);
+
+  // functions to display edit account fields forms
+  const handleShowEditProfilePic = () => {
     setEditProfilePic(!editProfilePic);
   };
-
-  const handleEditUsername = () => {
+  const handleShowEditUsername = () => {
     setEditUsername(!editUsername);
   };
-  const handleEditEmail = () => {
+  const handleShowEditEmail = () => {
     setEditEmail(!editEmail);
+  };
+
+  // functions to edit account fields forms
+  //   const handleEditProfilePic = (e) => {
+  //     setEditProfilePic(e.target.value);
+  //     console.log()
+  //   };
+  const handleEditUsernameOnChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleEditEmailOnChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  // handle submitting forms
+  const handleSubmitUsername = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/users/${user._id}/update-username/`,
+        {
+          username: username,
+        }
+      );
+      console.log(res.data);
+      setUser(res.data);
+      setEditUsername(!editUsername);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
@@ -35,28 +74,38 @@ function SettingPage() {
       <h2>Settings</h2>
       <div className="userDisplay">
         <img
-          onClick={handleEditProfilePic}
+          onClick={handleShowEditProfilePic}
           src={TempProfilePic}
           alt="profile pic"
         />
         {editUsername ? (
-          <form>
+          <form onSubmit={handleSubmitUsername}>
             {" "}
-            <input type="text" />
+            <input
+              onChange={handleEditUsernameOnChange}
+              type="text"
+              value={username}
+            />
+            <button type="submit">submit</button>
           </form>
         ) : (
           <p>{user.username}</p>
         )}
-        <FontAwesomeIcon icon={faPen} onClick={handleEditUsername} />
+        <FontAwesomeIcon icon={faPen} onClick={handleShowEditUsername} />
       </div>
       <form>
         <label> Email </label>
         {editEmail ? (
-          <input type="text" placeholder="Email" />
+          <input
+            onChange={handleEditEmailOnChange}
+            type="text"
+            placeholder="Email"
+            value={email}
+          />
         ) : (
           <p>{user.email}</p>
         )}
-        <FontAwesomeIcon icon={faPen} onClick={handleEditEmail} />
+        <FontAwesomeIcon icon={faPen} onClick={handleShowEditEmail} />
       </form>
       <form>
         <label>
