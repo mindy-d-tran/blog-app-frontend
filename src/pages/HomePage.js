4361812a6e16531cf3edd04a17bfedb4a2b6e602
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 23:19:14 -0500

    fetching posts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,21 @@
 import NavBar from "../components/NavBar";
 import PostList from "../components/PostList";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function HomePage() {
-  return (
-    <>
-      <PostList />
-    </>
-  );
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("http://localhost:4000/api/posts");
+      setPosts(res.data);
+      // console.log(res.data);
+    };
+    fetchData();
+  }, []);
+  return <PostList posts={posts} />;
 }
 
 export default HomePage;
