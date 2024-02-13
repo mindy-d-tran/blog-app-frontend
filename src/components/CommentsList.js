2ec929f25581ebc29b84bf2d7e87595ac9ada93e
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 16:31:59 -0500

    rearrangling imports

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,8 +1,11 @@
-import { CommentContext } from "../context/CommentContext";
-import Comment from "./Comment";
-import NewCommentForm from "./NewCommentForm";
 import { useContext } from "react";
 
+// importing components
+import Comment from "./Comment";
+import NewCommentForm from "./NewCommentForm";
+
+// importing context
+import { CommentContext } from "../context/CommentContext";
 
 function CommentsList({  postID }) {
   const commentsCtx = useContext(CommentContext);
