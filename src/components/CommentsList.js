617977ba9d78e9f3cc289e8b5f1d8224e718f1ed
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 15:50:15 -0500

    import new comment form

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,7 +1,9 @@
 import Comment from "./Comment";
+import NewCommentForm from "./NewCommentForm";
 function CommentsList({ comments }) {
   return (
     <section className="comment-list">
+      <NewCommentForm/>
       {comments.map((c) => (
         <Comment key={c._id} comment={c} />
       ))}
