a91f1b1763e0875a5db461f0adfae3a978825767
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 11:27:38 -0500

    copy and past code from Post component

---
+++ src/components/PostDetail.js
@@ -0,0 +1,101 @@
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { UserContext } from "../context/UserContext";
+
+//components
+import Comment from "./Comment";
+
+// importing icons from font awsome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart as fasHeart,
+  faComment,
+  faShareFromSquare,
+} from "@fortawesome/free-regular-svg-icons";
+
+function PostDetail({ id }) {
+
+  const userCtx = useContext(UserContext);
+  const { user } = userCtx;
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const res = await axios.get(`http://localhost:4000/api/posts/${id}/`);
+        console.log(res.data);
+        setPost(res.data);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleOnClickLike = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/posts/${post._id}/unlike`,
+        { user_id: user._id }
+      );
+      console.log(res.data);
+      setPost(res.data.post);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleOnClickUnlike = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/posts/${post._id}/like`,
+        { user_id: user._id }
+      );
+      console.log(res.data);
+      setPost(res.data.post);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      {post ? (
+        <div className="post">
+          <section className="post-body">
+            <img className="user-post-icon" src="" alt="user icon" />
+            <p>{post.user_id.username}</p>
+            <h2>{post.post_title}</h2>
+            {post.post_content.img ? (
+              <img className="post-img" src="" alt="post img" />
+            ) : (
+              <></>
+            )}
+
+            <p className="post-content">{post.post_content.text}</p>
+            <p>hash tags</p>
+            <section className="postbuttons">
+              {post.post_likes.find((p) => p.user_id === user._id) ? (
+                <FontAwesomeIcon onClick={handleOnClickLike} icon={faHeart} />
+              ) : (
+                <FontAwesomeIcon
+                  onClick={handleOnClickUnlike}
+                  icon={fasHeart}
+                />
+              )}
+              <FontAwesomeIcon icon={faComment} />
+              <FontAwesomeIcon icon={faShareFromSquare} />
+            </section>
+          </section>
+
+          <section className="comment-list">
+            <Comment />
+          </section>
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
+    </>
+  );
+}
+
+export default PostDetail;
