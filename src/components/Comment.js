92115067d15395cc2135473d1d0c57b5974b0e23
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 13:53:48 -0500

    add functionalty to like or unlike comment copy code from post

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,15 +1,60 @@
-function Comment({comment}) {
-  console.log(comment)
+import { useState, useContext } from "react";
+import axios from "axios";
+import { UserContext } from "../context/UserContext";
+// importing icons from font awsome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart as fasHeart,
+  faComment,
+  faShareFromSquare,
+} from "@fortawesome/free-regular-svg-icons";
+
+function Comment({ comment }) {
+  const userCtx = useContext(UserContext);
+  const { user } = userCtx;
+  const [commentState, setCommentState] = useState(comment);
+
+  // console.log(comment);
+
+  const handleOnClickLike = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/comments/${commentState._id}/unlike`,
+        { user_id: user._id }
+      );
+      console.log(res.data);
+      setCommentState(res.data.comment);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleOnClickUnlike = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:4000/api/comments/${commentState._id}/like`,
+        { user_id: user._id }
+      );
+      console.log(res.data);
+      setCommentState(res.data.comment);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="comment">
-      <h3>Comment</h3>
       <img className="user-comment-icon" src="" alt="user icon" />
-      <p>Username</p>
-      <p>Comment content</p>
+      <p>{commentState.user_id.username}</p>
+      <p>{commentState.comment_content}</p>
       <section className="postbuttons">
-        <button className="like">like</button>
-        <button className="comment">comment</button>
-        <button className="share">share</button>
+        {commentState.comment_like.find((c) => c.user_id === user._id) ? (
+          <FontAwesomeIcon onClick={handleOnClickLike} icon={faHeart} />
+        ) : (
+          <FontAwesomeIcon onClick={handleOnClickUnlike} icon={fasHeart} />
+        )}
+        <FontAwesomeIcon icon={faComment} />
+        <FontAwesomeIcon icon={faShareFromSquare} />
       </section>
     </div>
   );
