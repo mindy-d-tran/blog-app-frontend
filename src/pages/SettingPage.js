1c9e02fd383053687c5cfd355d953283fdc04995
Mindy D Tran <mindytran98@gmail.com>
2024-02-12 19:15:02 -0500

    add temp functions to change state of edit

--- src/pages/SettingPage.js
+++ src/pages/SettingPage.js
@@ -4,55 +4,59 @@ import { useContext, useState } from "react";
 // importing icons from font awsome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
+import TempProfilePic from "../assets/cat_logo_transparent.png";
 
-import './SettingPage.css';
+import "./SettingPage.css";
 // const API_KEY ="live_q6c3JrM10gOEfZinLjnSxMkjOdqR1bToljv8rziaj3d2JoXRsFRvb1AaEUzKKVQ2";
 // const url = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`;
 
 function SettingPage() {
   const userCtx = useContext(UserContext);
   const { user } = userCtx;
-  console.log(user);
+  //   console.log(user);
 
   const [editProfilePic, setEditProfilePic] = useState(false);
   const [editUsername, setEditUsername] = useState(false);
   const [editEmail, setEditEmail] = useState(false);
 
   const handleEditProfilePic = () => {
-    setEditProfilePic(true);
-    console.log(editProfilePic);
+    setEditProfilePic(!editProfilePic);
   };
+
   const handleEditUsername = () => {
     setEditUsername(!editUsername);
-
+  };
+  const handleEditEmail = () => {
+    setEditEmail(!editEmail);
   };
 
   return (
-    <div>
+    <div className="settings">
       <h2>Settings</h2>
-      <div>
-        <img onClick={handleEditProfilePic} src="" alt="profile pic" />
+      <div className="userDisplay">
+        <img
+          onClick={handleEditProfilePic}
+          src={TempProfilePic}
+          alt="profile pic"
+        />
         {editUsername ? (
           <form>
             {" "}
             <input type="text" />
           </form>
         ) : (
-          <p>
-            {user.username}
-          </p>
+          <p>{user.username}</p>
         )}
-        <FontAwesomeIcon icon={faPen} onClick={handleEditUsername}/>
+        <FontAwesomeIcon icon={faPen} onClick={handleEditUsername} />
       </div>
       <form>
         <label> Email </label>
         {editEmail ? (
           <input type="text" placeholder="Email" />
         ) : (
-          <p>
-            {user.email} <FontAwesomeIcon icon={faPen} />
-          </p>
+          <p>{user.email}</p>
         )}
+        <FontAwesomeIcon icon={faPen} onClick={handleEditEmail} />
       </form>
       <form>
         <label>
