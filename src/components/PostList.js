0e822d800395f8e706623fd81529bc67105cc8bb
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 19:57:06 -0500

    import posts context

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,15 @@
 import Post from "./Post";
+import { PostsContext } from "../context/PostsContext";
+import { useContext } from "react";
+import NewCommentForm from "./NewPostForm";
 
-function PostList({ posts }) {
+function PostList() {
+  const postsCtx = useContext(PostsContext);
+  const {posts} = postsCtx;
+
   return (
     <section className='post-list'>
+      <NewCommentForm/>
       {posts.map((post) => (
         <Post key={post._id} post={post} />
       ))}
