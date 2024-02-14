151a3800ecf7386145e0b7a6f0cb16f2720bd74e
Mindy D Tran <mindytran98@gmail.com>
2024-02-14 10:02:26 -0500

    add delete function for posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,30 @@
 // importing icons from font awsome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faChartSimple } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart,
+  faChartSimple,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   faHeart as fasHeart,
   faComment,
   faShareFromSquare,
 } from "@fortawesome/free-regular-svg-icons";
 import { useContext, useState } from "react";
+import ProfilePic from "../assets/cat_logo_transparent.png";
+
 import axios from "axios";
 import { UserContext } from "../context/UserContext";
+import { PostsContext } from "../context/PostsContext";
 
 import { Link } from "react-router-dom";
-import ProfilePic from "../assets/cat_logo_transparent.png";
 
 function Post({ post }) {
   const userCtx = useContext(UserContext);
   const { user } = userCtx;
+
+  const postsCtx = useContext(PostsContext);
+  const { setPosts } = postsCtx;
 
   // need this to make the like or unlike button change icon
   const [postState, setPostState] = useState(post);
@@ -44,6 +53,20 @@ function Post({ post }) {
       console.log(error);
     }
   };
+  const handleOnClickDelete = async () => {
+    try {
+      const res = await axios.delete(
+        `https://blog-app-0no1.onrender.com/api/posts/${postState._id}/`
+      );
+      const resPosts = await axios.get(
+        `https://blog-app-0no1.onrender.com/api/posts`
+      );
+      // console.log(res.data);
+      setPosts(resPosts.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       {postState ? (
@@ -51,13 +74,20 @@ function Post({ post }) {
           <div className="userDisplay-small">
             <img className="user-post-icon" src={ProfilePic} alt="user icon" />
             <p>{postState.user_id.username}</p>
+            {postState.user_id._id === user._id && (
+              <FontAwesomeIcon
+                onClick={handleOnClickDelete}
+                icon={faTrash}
+                size="sm"
+              />
+            )}
           </div>
 
           <Link to={`/post/${post._id}`}>
             <h2>{postState.post_title}</h2>
             <section className="post-img-gallery">
               {postState.post_content.img.length > 0 ? (
-                postState.post_content.img.map((img,i) => (
+                postState.post_content.img.map((img, i) => (
                   <img key={i} className="post-img" src={img} alt="post img" />
                 ))
               ) : (
