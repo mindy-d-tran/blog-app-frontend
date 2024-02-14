23897bdfa3a3cf45a4e12db141abc4d727d5ea6f
Mindy D Tran <mindytran98@gmail.com>
2024-02-13 23:04:18 -0500

    can create new posts

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -3,45 +3,62 @@ import { useState, useContext } from "react";
 
 import { UserContext } from "../context/UserContext";
 import { PostsContext } from "../context/PostsContext";
+import ProfilePic from "../assets/cat_logo_transparent.png";
 
-function NewCommentForm() {
+function NewPostForm() {
   const userCtx = useContext(UserContext);
   const { user } = userCtx;
 
   const postsCtx = useContext(PostsContext);
-  const { posts } = postsCtx;
+  const { setPosts } = postsCtx;
 
-  const [newPost, setNewPost] = useState("");
+  const [newPostContent, setNewPostContent] = useState("");
+  const [newPostTitle, setNewPostTitle] = useState("");
 
-//   const handleOnSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       console.log(newComment);
-//       const res = await axios.post(`http://localhost:4000/api/posts`, {
-//         user_id: user._id,
-//         post_id: postID,
-//         comment_content: newComment,
-//       });
-//       console.log(res.data);
-
-//       setComments(res.data);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`http://localhost:4000/api/posts`, {
+        user_id: user._id,
+        post_content: {
+          text: newPostContent,
+        },
+        post_title: newPostTitle,
+      });
+      console.log(res.data);
+      const resPosts = await axios.get(`http://localhost:4000/api/posts`);
+      setPosts(resPosts.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <form >
-      <input
-        onChange={(e) => {
-          setNewPost(e.target.value);
-        }}
-        type="text"
-        value={newPost}
-      />
-      <button>submit</button>
-    </form>
+    <div className="create-post-container">
+      <div className="userDisplay-small">
+        <img className="user-post-icon" src={ProfilePic} alt="user icon" />
+      </div>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          onChange={(e) => {
+            setNewPostTitle(e.target.value);
+          }}
+          type="text"
+          value={newPostTitle}
+          placeholder="Title"
+        />
+        <input
+          onChange={(e) => {
+            setNewPostContent(e.target.value);
+          }}
+          type="text"
+          value={newPostContent}
+          placeholder="Text"
+        />
+        <button>Post</button>
+      </form>
+    </div>
   );
 }
 
-export default NewCommentForm;
+export default NewPostForm;
